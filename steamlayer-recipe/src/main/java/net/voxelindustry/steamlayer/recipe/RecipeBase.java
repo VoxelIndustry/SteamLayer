7ad9a848286d66f1c3d8f660d3bae81900bdb1dd
Ourten <ourtencoop@gmail.com>
2020-07-12 23:48:46 +0200

    Change RecipeBase inputs and outputs to Multimaps

--- steamlayer-recipe/src/main/java/net/voxelindustry/steamlayer/recipe/RecipeBase.java
+++ steamlayer-recipe/src/main/java/net/voxelindustry/steamlayer/recipe/RecipeBase.java
@@ -1,20 +1,20 @@
 package net.voxelindustry.steamlayer.recipe;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
 import net.voxelindustry.steamlayer.recipe.ingredient.RecipeIngredient;
 
-import java.util.IdentityHashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 public abstract class RecipeBase
 {
-    protected final Map<Class<?>, DefaultedList<RecipeIngredient<?>>> inputs  = new IdentityHashMap<>();
-    protected final Map<Class<?>, DefaultedList<RecipeIngredient<?>>> outputs = new IdentityHashMap<>();
-
-    private int[] essentialInputsCache;
+    protected final Multimap<Class<?>, RecipeIngredient<?>> inputs  = Multimaps.newListMultimap(Maps.newIdentityHashMap(), ArrayList::new);
+    protected final Multimap<Class<?>, RecipeIngredient<?>> outputs = Multimaps.newListMultimap(Maps.newIdentityHashMap(), ArrayList::new);
 
     public boolean hasInputType(Class<?> input)
     {
@@ -26,37 +26,52 @@ public abstract class RecipeBase
         return outputs.containsKey(output);
     }
 
-    public <T> DefaultedList<RecipeIngredient<T>> getRecipeInputs(Class<T> clazz)
+    @SuppressWarnings("unchecked")
+    public <T> List<RecipeIngredient<T>> getRecipeInputs(Class<T> clazz)
     {
         if (hasInputType(clazz))
-            return (DefaultedList<RecipeIngredient<T>>) (Object) inputs.get(clazz);
+            return (List<RecipeIngredient<T>>) (Object) inputs.get(clazz);
         return DefaultedList.of();
     }
 
-    public <T> DefaultedList<RecipeIngredient<T>> getRecipeOutputs(Class<T> clazz)
+    @SuppressWarnings("unchecked")
+    public <T> List<RecipeIngredient<T>> getRecipeOutputs(Class<T> clazz)
     {
         if (hasOutputType(clazz))
-            return (DefaultedList<RecipeIngredient<T>>) (Object) outputs.get(clazz);
+            return (List<RecipeIngredient<T>>) (Object) outputs.get(clazz);
         return DefaultedList.of();
     }
 
-    public Optional<DefaultedList<ItemStack>> getRemainingItems()
+    public Optional<List<ItemStack>> getRemainingItems()
     {
         return Optional.empty();
     }
 
-    /**
-     * Used to retrieve inputs slots to be validated for recipe retrieval by the Category
-     *
-     * @return array of the slots
-     * @see net.voxelindustry.steamlayer.recipe.category.RecipeCategory#findRecipes(Object...)
-     */
-    public int[] getEssentialInputSlots()
+    public abstract int getTime();
+
+    @SafeVarargs
+    protected final <T> void addInputs(Class<T> inputType, RecipeIngredient<T> firstInput, RecipeIngredient<T>... inputs)
     {
-        if (essentialInputsCache == null)
-            essentialInputsCache = IntStream.range(0, inputs.size()).toArray();
-        return essentialInputsCache;
+        this.inputs.put(inputType, firstInput);
+        for (RecipeIngredient<T> input : inputs)
+            this.inputs.put(inputType, input);
     }
 
-    public abstract int getTime();
+    @SafeVarargs
+    protected final <T> void addOutputs(Class<T> outputType, RecipeIngredient<T> firstOutput, RecipeIngredient<T>... outputs)
+    {
+        this.outputs.put(outputType, firstOutput);
+        for (RecipeIngredient<T> output : outputs)
+            this.outputs.put(outputType, output);
+    }
+
+    protected final <T> void addInput(Class<T> inputType, RecipeIngredient<T> input)
+    {
+        inputs.put(inputType, input);
+    }
+
+    protected final <T> void addOutput(Class<T> outputType, RecipeIngredient<T> output)
+    {
+        outputs.put(outputType, output);
+    }
 }

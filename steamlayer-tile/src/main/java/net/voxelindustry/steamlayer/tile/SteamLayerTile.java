55497fb944ca110f6f2ac4ee47a4764e290a4e62
Ourten <ourtencoop@gmail.com>
2020-07-18 00:49:51 +0200

    Fix ILoadable tile not using appropriate event

--- steamlayer-tile/src/main/java/net/voxelindustry/steamlayer/tile/SteamLayerTile.java
+++ steamlayer-tile/src/main/java/net/voxelindustry/steamlayer/tile/SteamLayerTile.java
@@ -1,14 +1,17 @@
 package net.voxelindustry.steamlayer.tile;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.voxelindustry.steamlayer.tile.event.TileTickHandler;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerBlockEntityEvents;
 
 public class SteamLayerTile implements ModInitializer
 {
     @Override
     public void onInitialize()
     {
-        ServerTickEvents.END_SERVER_TICK.register(server -> TileTickHandler.tick());
+        ServerBlockEntityEvents.BLOCK_ENTITY_LOAD.register((blockEntity, serverWorld) ->
+        {
+            if (blockEntity instanceof ILoadable)
+                ((ILoadable) blockEntity).load();
+        });
     }
 }
